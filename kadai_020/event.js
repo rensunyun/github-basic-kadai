c5d12cfc282007c3796243a93c3cd7782158122f
rensunyun <nononononodadadadada@yahoo.co.jp>
2025-05-04 11:25:56 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+// ボタンの要素とテキスト要素を取得
+const button = document.getElementById("btn");
+const text = document.getElementById("text");
+
+// ボタンがクリックされたときの処理
+button.addEventListener("click", function () {
+  text.textContent = "ボタンをクリックしました";
+});
